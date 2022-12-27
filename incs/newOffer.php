5421111e41109d0e93343d4fee1a3e0788880fcc
Alexander <95166550+DericcoM@users.noreply.github.com>
2022-12-27 04:55:06 +0300

    Update newOffer.php

--- incs/newOffer.php
+++ incs/newOffer.php
@@ -2,12 +2,20 @@
 
 $text = filter_var(trim($_POST['message']), FILTER_SANITIZE_STRING);
 $user = $_COOKIE['user'];
-$image = addslashes(file_get_contents($_FILES['file']['tmp_name']));
-//addslashes  Возвращает строку с обратным слешом перед символами, которые нужно экранировать.
-//file_get_contents() возвращает содержимое файла в строке, начиная с указанного смещения offset и до length байт
 
-$mysql_posts = new mysqli('localhost', 'root', 'root', 'posts');
-$mysql_posts->query("INSERT INTO `user_post` (`user`,`text`, `likes`, `dislikes`, `image`) VALUES ('$user', '$text', '0', '0', '$image')");
+if ($_FILES && $_FILES['file']['error']== UPLOAD_ERR_OK) {
+    $image = addslashes(file_get_contents($_FILES['file']['tmp_name']));
+    //addslashes  Возвращает строку с обратным слешом перед символами, которые нужно экранировать.
+    //file_get_contents() возвращает содержимое файла в строке, начиная с указанного смещения offset и до length байт
+    $mysql_posts = new mysqli('localhost', 'root', 'root', 'login_form');
+    $mysql_posts->query("INSERT INTO `posts` (`user`,`text`, `likes`, `dislikes`, `image`) VALUES ('$user', '$text', '0', '0', '$image')");
 
     header('Location: /phoenix/main.php');
+
+} else {
+    $mysql_posts = new mysqli('localhost', 'root', 'root', 'login_form');
+    $mysql_posts->query("INSERT INTO `posts` (`user`,`text`, `likes`, `dislikes`, `image`) VALUES ('$user', '$text', '0', '0', '')");
+
+    header('Location: /phoenix/main.php');
+}
 ?>
